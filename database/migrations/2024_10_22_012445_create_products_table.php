95cf709158767aa772246d4456c7c5f1602fc754
aimanyusufw <aimanyusufdev@gmail.com>
2024-10-22 08:48:08 +0700

    ref : refactoring typo in title schema table default value

--- database/migrations/2024_10_22_012445_create_products_table.php
+++ database/migrations/2024_10_22_012445_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable()->default("Unilited product");
+            $table->string('title')->nullable()->default("Untitled product");
             $table->string('slug')->nullable();
             $table->string('excerpt')->nullable();
             $table->text('description')->nullable();
