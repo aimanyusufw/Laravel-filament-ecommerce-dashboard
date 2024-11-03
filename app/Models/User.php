24d852d35be9b950ff0a0258887203978121cbd2
aimanyusufw <aimanyusufdev@gmail.com>
2024-11-03 10:32:33 +0700

    ref : add relation to cart model

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
