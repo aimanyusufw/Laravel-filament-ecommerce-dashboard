3ed21f4130f74b3963fc5aa04e3ec6f2aceb378b
aimanyusufw <aimanyusufdev@gmail.com>
2024-11-05 13:44:45 +0700

    ref : reafcto role resource

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -26,29 +26,29 @@ class RoleResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Section::make()->schema(
-                    [
-                        Forms\Components\TextInput::make('name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('guard_name')
-                            ->required()
-                            ->unique()
-                            ->maxLength(255),
-                        Forms\Components\Textarea::make('description')
-                            ->maxLength(255)
-                            ->columnSpanFull()
-                            ->rows(5),
-                    ]
-                )->columns(['sm' => 2])->columnSpan(2),
-                Forms\Components\Section::make("Time Stamps")->schema(
-                    [
-                        Forms\Components\Placeholder::make('created_at')
-                            ->content(fn(?Role $record) => $record ? $record->created_at->diffForHumans() : '-'),
-                        Forms\Components\Placeholder::make('updated_at')
-                            ->content(fn(?Role $record) => $record ? $record->updated_at->diffForHumans() : '-')
-                    ]
-                )->columnSpan(1)
+                Forms\Components\Section::make()->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Select::make('guard_name')
+                        ->options([
+                            "web" => "Web",
+                            "api" => "Api"
+                        ])
+                        ->default('web')
+                        ->required(),
+                    Forms\Components\Textarea::make('description')
+                        ->rows(5)
+                        ->columnSpanFull()
+                        ->required()
+                        ->maxLength(255),
+                ])->columns(['sm' => 2])->columnSpan(2),
+                Forms\Components\Section::make("Time stamps")->schema([
+                    Forms\Components\Placeholder::make("created_at")
+                        ->content(fn(?Role $record): String => $record ? $record->created_at->diffForHumans() : "-"),
+                    Forms\Components\Placeholder::make("updated_at")
+                        ->content(fn(?Role $record): String => $record ? $record->updated_at->diffForHumans() : "-")
+                ])->columnSpan(1)
             ])->columns(3);
     }
 
@@ -63,10 +63,11 @@ class RoleResource extends Resource
                 Tables\Columns\TextColumn::make('description')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
-                    ->since()
-                    ->sortable(),
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
-                    ->date()
+                    ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
@@ -75,15 +76,12 @@ class RoleResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 // Tables\Actions\BulkActionGroup::make([
                 //     Tables\Actions\DeleteBulkAction::make(),
                 // ]),
             ])
-            ->emptyStateIcon('heroicon-o-shield-check')
-            ->emptyStateDescription('Create role and detail role data.')
             ->emptyStateActions([
                 Action::make('create')
                     ->label('Create role')
