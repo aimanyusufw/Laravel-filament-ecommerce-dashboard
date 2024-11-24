d1b329dd9c8b47f2596ba05f98e9f3161fc39d0e
aimanyusufw <aimanyusufdev@gmail.com>
2024-11-24 22:47:12 +0700

    ref : refacto oder resource view

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Support\RawJs;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -23,66 +24,152 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('status')
-                    ->maxLength(255)
-                    ->default(1),
-                Forms\Components\TextInput::make('shipping_detail'),
-                Forms\Components\TextInput::make('shipping_address_detail'),
-                Forms\Components\TextInput::make('subtotal')
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('shipping_cost')
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('tax')
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('grand_total')
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\DatePicker::make('order_date'),
-                Forms\Components\TextInput::make('order_code')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('resi_code')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('notes')
-                    ->maxLength(255),
-            ]);
+                Forms\Components\Tabs::make('Tabs')
+                    ->tabs([
+                        Forms\Components\Tabs\Tab::make('Order detail')
+                            ->schema([
+                                Forms\Components\TextInput::make('subtotal')
+                                    ->prefix('Rp')
+                                    ->mask(RawJs::make('$money($input)'))
+                                    ->stripCharacters(',')
+                                    ->numeric()
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_cost')
+                                    ->prefix('Rp')
+                                    ->mask(RawJs::make('$money($input)'))
+                                    ->stripCharacters(',')
+                                    ->numeric()
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('tax')
+                                    ->prefix('Rp')
+                                    ->mask(RawJs::make('$money($input)'))
+                                    ->stripCharacters(',')
+                                    ->numeric()
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('grand_total')
+                                    ->prefix('Rp')
+                                    ->mask(RawJs::make('$money($input)'))
+                                    ->stripCharacters(',')
+                                    ->numeric()
+                                    ->readOnly(),
+                                Forms\Components\Select::make('status')
+                                    ->options([
+                                        '1' => 'Pending payment',
+                                        '2' => 'Processing',
+                                        '3' => 'Ready to ship',
+                                        '4' => 'Shipped',
+                                        '5' => 'Completed',
+                                        '6' => 'Cancelled',
+                                    ]),
+                                Forms\Components\TextInput::make('user.name')
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('order_date')
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('resi_code')
+                                    ->readOnly(),
+                                Forms\Components\Textarea::make('notes')
+                                    ->rows(5)
+                                    ->readOnly(),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Shipping Address detail')
+                            ->schema([
+                                Forms\Components\TextInput::make('shipping_address_detail.title')
+                                    ->label("Title")
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_address_detail.shipping_name')
+                                    ->label("Name")
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_address_detail.shipping_phone')
+                                    ->label("Phone")
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_address_detail.shipping_province.name')
+                                    ->label("Province")
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_address_detail.shipping_city.name')
+                                    ->label("City")
+                                    ->readOnly(),
+                                Forms\Components\Textarea::make('shipping_address_detail.shipping_address')
+                                    ->rows(4)
+                                    ->maxLength(125)
+                                    ->label("Address")
+                                    ->readOnly(),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Shipping detail')
+                            ->schema([
+                                Forms\Components\TextInput::make('shipping_detail.service')
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_detail.description')
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_detail.etd')
+                                    ->readOnly(),
+                                Forms\Components\TextInput::make('shipping_detail.cost')
+                                    ->prefix('Rp')
+                                    ->mask(RawJs::make('$money($input)'))
+                                    ->stripCharacters(',')
+                                    ->numeric()
+                                    ->readOnly(),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('Product items')
+                            ->schema([
+                                Forms\Components\Repeater::make('orderItmes')
+                                    ->relationship('orderItmes')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('product_title'),
+                                    ])->columns(['sm' => 2])->columnSpanFull()
+                            ]),
+                    ])->columns(
+                        ['sm' => 2]
+                    )->columnSpan(2),
+                Forms\Components\Section::make("Time stamps")->schema([
+                    Forms\Components\Placeholder::make("created_at")
+                        ->content(fn(?Order $record): String => $record ? $record->created_at->diffForHumans() : "-"),
+                    Forms\Components\Placeholder::make("updated_at")
+                        ->content(fn(?Order $record): String => $record ? $record->updated_at->diffForHumans() : "-")
+                ])->columnSpan(1)
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
+                Tables\Columns\TextColumn::make('order_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.name')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('subtotal')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('shipping_cost')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('tax')
-                    ->numeric()
-                    ->sortable(),
+                    ->formatStateUsing(function ($state) {
+                        return match ($state) {
+                            '1' => 'Pending payment',
+                            '2' => 'Processing',
+                            '3' => 'Ready to ship',
+                            '4' => 'Shipped',
+                            '5' => 'Completed',
+                            '6' => 'Cancelled',
+                            default => 'Unknown',
+                        };
+                    })
+                    ->badge()
+                    ->colors([
+                        'primary' => '1',
+                        'gray' => '2',
+                        'warning' => '3',
+                        'info' => '4',
+                        'success' => '5',
+                        'danger' => '6'
+                    ]),
                 Tables\Columns\TextColumn::make('grand_total')
+                    ->prefix('Rp ')
+                    ->money('Rp', locale: 'id')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('order_date')
                     ->date()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('order_code')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('resi_code')
-                    ->searchable(),
                 Tables\Columns\TextColumn::make('notes')
-                    ->searchable(),
+                    ->searchable()
+                    ->default("-"),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -96,12 +183,12 @@ class OrderResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ])
             ->emptyStateIcon('heroicon-o-truck')
             ->emptyStateDescription('You just only can update and delete data.');
